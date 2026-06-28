91570cc8705b42e9aaaea00e2e099c72f5ab4a21
Kellojo <12833426+Kellojo@users.noreply.github.com>
2026-06-28 18:23:01 +0200

    Added github link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,21 +16,45 @@
       <Icon icon="solar:chart-bold-duotone" />
       Dashboard
     </a>
-    <a href="/providers" class:active={page.url.pathname.startsWith("/providers")} title="Providers">
+    <a
+      href="/providers"
+      class:active={page.url.pathname.startsWith("/providers")}
+      title="Providers"
+    >
       <Icon icon="mynaui:api-solid" />
       Providers
     </a>
-    <a href="/model-mappings" class:active={page.url.pathname.startsWith("/model-mappings")} title="Model Mappings">
+    <a
+      href="/model-mappings"
+      class:active={page.url.pathname.startsWith("/model-mappings")}
+      title="Model Mappings"
+    >
       <Icon icon="weui:setting-filled" />
       Model Mappings
     </a>
-    <a href="/virtual-keys" class:active={page.url.pathname.startsWith("/virtual-keys")} title="Virtual Keys">
+    <a
+      href="/virtual-keys"
+      class:active={page.url.pathname.startsWith("/virtual-keys")}
+      title="Virtual Keys"
+    >
       <Icon icon="tabler:key-filled" />
       Virtual Keys
     </a>
-    <a href="/docs" class:active={page.url.pathname.startsWith("/docs")} title="Get Started">
+    <a
+      href="/docs"
+      class:active={page.url.pathname.startsWith("/docs")}
+      title="Get Started"
+    >
       <Icon icon="material-symbols:book" />
       Get Started
+    </a>
+    <a
+      href="https://github.com/Kellojo/ai-proxy"
+      target="_blank"
+      rel="noopener noreferrer"
+      title="GitHub"
+    >
+      <Icon icon="tabler:brand-github" />
     </a>
   </svelte:fragment>
 </Navbar>
